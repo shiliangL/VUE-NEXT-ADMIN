4b131f075767807e07d90342002531f0bae3ab31
shiliangL <shiliangwei@wintelia.com>
2019-07-19 11:35:39 +0800

    添加banner

--- src/_c/AppTopNavBar/index.js
+++ src/_c/AppTopNavBar/index.js
@@ -8,8 +8,8 @@ export default class AppTopNavBar extends Component {
           <a href="#" className="brand-logo">shiliangL</a>
           <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="sass.html">Sass</a></li>
-            <li><a href="badges.html">Components</a></li>
+            <li><a href="sass.html">Flutter</a></li>
+            <li><a href="badges.html">Node</a></li>
             <li><a href="collapsible.html">JavaScript</a></li>
           </ul>
         </div>
