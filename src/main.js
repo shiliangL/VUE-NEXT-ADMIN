90ebf356ad4371d107c53660774376a99b1a2ce4
shiliangL <shiliangwei@wintelia.com>
2019-08-07 14:43:36 +0800

    切换3.0脚手架+

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import './styles/index.scss'
 import Vue from 'vue'
 import router from './router'
 
@@ -5,6 +6,18 @@ import './cube-ui'
 import './icons'
 
 import App from './App.vue'
+
+import FastClick from 'fastclick'
+
+import { mockXHR } from '../mock'
+
+Vue.config.productionTip = false
+
+//  hack the active pseudo-classes failure caused by -webkit-overflow-scrolling touch
+FastClick.attach(document.body)
+if (process.env.NODE_ENV === 'production') {
+  mockXHR()
+}
 
 Vue.config.productionTip = false
 
